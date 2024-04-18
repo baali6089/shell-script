31e37a1389dda401eb6fd948ca336de1922f2181
baali6089 <harish6089@gmail.com>
2024-04-18 23:41:47 +0530

    variables

--- 06-array.sh
+++ 06-array.sh
@@ -2,6 +2,12 @@
 
 movies=("rrr" "nbk" "modi")
 
+#size of above array is 3
+
+#index are 0,1,2
+
+#list always start with 0.
+
 echo "first movie is: ${movies[0]}"
 
 echo "first movie is: ${movies[1]}"
