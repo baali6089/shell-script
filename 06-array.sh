ceff7fe9bd5dfe266914984eaee5dbc2e9c7c1cc
baali6089 <harish6089@gmail.com>
2024-04-18 18:16:41 +0530

    variables

--- 06-array.sh
+++ 06-array.sh
@@ -2,8 +2,8 @@
 
 movies=("rrr" "nbk" "modi")
 
-echo "first movie is: ${movie[0]}"
+echo "first movie is: ${movies[0]}"
 
-echo "first movie is: ${movie[1]}"
+echo "first movie is: ${movies[1]}"
 
-echo "first movie is: ${movie[@]}"
+echo "first movie is: ${movies[@]}"
