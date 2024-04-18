d4f0eb654afa25a03fa283ecb6161790aee8f352
baali6089 <harish6089@gmail.com>
2024-04-18 23:59:49 +0530

    variables

--- 07-special-variable.sh
+++ 07-special-variable.sh
@@ -5,4 +5,5 @@ echo "number of variables passed: $#"
 echo "script name: $0"
 echo "current working directory: $PWD"
 echo "home directory of current user: $HOME"
-echo "which user is running this script: $USER
+echo "which user is running this script: $USER"
+echo "hostname: $HOSTNAME"
