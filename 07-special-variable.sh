e039a49eba1d0b7c85e730e02b8aed46393dd1fb
baali6089 <harish6089@gmail.com>
2024-04-19 00:07:25 +0530

    variables

--- 07-special-variable.sh
+++ 07-special-variable.sh
@@ -7,3 +7,6 @@ echo "current working directory: $PWD"
 echo "home directory of current user: $HOME"
 echo "which user is running this script: $USER"
 echo "hostname: $HOSTNAME"
+echo "process ID of the current shell script: $$"
+sleep 60
+echo "process ID of last background command: $!"
