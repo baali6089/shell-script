b2af8d9340f4ff1f99a24fa0eda3a857f2a5609a
baali6089 <harish6089@gmail.com>
2024-04-22 21:09:10 +0530

    install

---
+++ 12-functionnns.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOFFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.LOG
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+         echo "$2....failure"
+         exit 1
+    else
+         echo "$2....success"   
+    fi       
+}
+
+if [ $USERID -ne 0 ]
+then 
+     echo "please run this script with root access."
+     exit 1 #manually exit .if error comes
+else
+     echo "you are super user" 
+fi
+
+dnf install mysql -y &>>LOGFILE
+VALIDATE $? "installing mysql"
+
+dnf install git -y &>>LOGFILE
+VALIDATE $? "installing git"
