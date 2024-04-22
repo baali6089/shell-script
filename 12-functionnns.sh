3e733e9b68f19e0557a1e3bb625e76b665c93449
baali6089 <harish6089@gmail.com>
2024-04-22 21:15:24 +0530

    install

--- 12-functionnns.sh
+++ 12-functionnns.sh
@@ -3,7 +3,7 @@
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOFFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.LOG
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 
 VALIDATE(){
@@ -24,8 +24,8 @@ else
      echo "you are super user" 
 fi
 
-dnf install mysql -y &>>LOGFILE
+dnf install mysql -y &>>$LOGFILE
 VALIDATE $? "installing mysql"
 
-dnf install git -y &>>LOGFILE
+dnf install git -y &>>$LOGFILE
 VALIDATE $? "installing git"
